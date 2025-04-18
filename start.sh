100f3072c529fae23327bdc08ea4c581583f48c6
Ankit-3010 <63722534+Ankit-3010@users.noreply.github.com>
2025-04-18 17:26:58 +0530

    edit start.sh

--- start.sh
+++ start.sh
@@ -4,19 +4,9 @@ echo "Starting HealthWise application..."
 echo "Setting up backend..."
 cd backend
 
-if [ ! -d "venv" ]; then
-    python3 -m venv venv
-fi
-
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-    source venv/Scripts/activate
-else
-    source venv/bin/activate
-fi
 
 pip3 install -r requirements.txt
 
-python3 import_notebook_model.py
 
 echo "Starting Flask backend on port 5001..."
 python3 app.py
